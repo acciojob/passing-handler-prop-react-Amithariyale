b294515938da7a9083dd7f06c6c2c1172f6c44ee
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-02-13 01:39:53 +0530

    solution

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
+import "./Child.css";
 
+const Selection = ({ applyColor }) => {
+  let [styles, updateSelectionStyle] = useState({ backgroundColor: "" });
+  return (
+    <div
+      onClick={() => applyColor(updateSelectionStyle)}
+      className="fix-box"
+      style={styles}
+    >
+      <h1 className="subheading">Selection</h1>
+    </div>
+  );
+};
+
+export default Selection;
